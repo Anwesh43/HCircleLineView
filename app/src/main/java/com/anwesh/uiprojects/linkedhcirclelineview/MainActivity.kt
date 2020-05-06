ccf8dfaf9bf2296af35a641e792f39e1894a8204
Anwesh43 <anweshthecool0@gmail.com>
2020-05-07 04:59:28 +0530

    adding app and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedhcirclelineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedhcirclelineview/MainActivity.kt
@@ -2,11 +2,18 @@ package com.anwesh.uiprojects.linkedhcirclelineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.hcirclelineview.HCircleLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        HCircleLineView.create(this)
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
