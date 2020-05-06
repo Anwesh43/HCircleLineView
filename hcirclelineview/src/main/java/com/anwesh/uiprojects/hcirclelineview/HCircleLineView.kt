80615ca8199f480fa73a88843c5516c891bfcea5
Anwesh43 <anweshthecool0@gmail.com>
2020-05-07 04:05:04 +0530

    adding HCircleLine class

--- hcirclelineview/src/main/java/com/anwesh/uiprojects/hcirclelineview/HCircleLineView.kt
+++ hcirclelineview/src/main/java/com/anwesh/uiprojects/hcirclelineview/HCircleLineView.kt
@@ -167,4 +167,27 @@ class HCircleLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class HCircleLine(var i : Int) {
+
+        private var curr : HCLNode = HCLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
