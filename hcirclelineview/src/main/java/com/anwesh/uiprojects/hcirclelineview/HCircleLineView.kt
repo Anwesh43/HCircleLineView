0ecca8ca22ce7b4d8f0f0dcdec9a712e24dbc2ba
Anwesh43 <anweshthecool0@gmail.com>
2020-05-07 03:49:16 +0530

    adding HCircleLineView class

--- hcirclelineview/src/main/java/com/anwesh/uiprojects/hcirclelineview/HCircleLineView.kt
+++ hcirclelineview/src/main/java/com/anwesh/uiprojects/hcirclelineview/HCircleLineView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawHCLNode(i : Int, scale : Float, paint : Paint) {
     drawHCircleLine(scale, size, paint)
     restore()
 }
+
+class HCircleLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
