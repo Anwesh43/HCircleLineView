3020270280236f66091bffb4c3834b7739662a6d
Anwesh43 <anweshthecool0@gmail.com>
2020-05-07 03:06:01 +0530

    adding constants and libraries

--- hcirclelineview/src/main/java/com/anwesh/uiprojects/hcirclelineview/HCircleLineView.kt
+++ hcirclelineview/src/main/java/com/anwesh/uiprojects/hcirclelineview/HCircleLineView.kt
@@ -4,3 +4,22 @@ package com.anwesh.uiprojects.hcirclelineview
  * Created by anweshmishra on 07/05/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val parts : Int= 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val rot : Float = 90f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+
